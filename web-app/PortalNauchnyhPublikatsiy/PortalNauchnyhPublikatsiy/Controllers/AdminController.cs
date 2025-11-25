4b1deeb0e3d5a0da7b100d4a9539a6c4a35f2b80
Сеноженский Вячеслав Владимирович <com.cerety.vepl555@gmail.com>
2025-11-26 02:44:33 +0300

    v2.7: Палень Админа 1.1

--- web-app/PortalNauchnyhPublikatsiy/PortalNauchnyhPublikatsiy/Controllers/AdminController.cs
+++ web-app/PortalNauchnyhPublikatsiy/PortalNauchnyhPublikatsiy/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using PortalNauchnyhPublikatsiy.Web.Models;
 using PortalNauchnyhPublikatsiy.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace PortalNauchnyhPublikatsiy.Web.Controllers
 {
@@ -37,6 +38,66 @@ namespace PortalNauchnyhPublikatsiy.Web.Controllers
             }
 
             return View(userViewModels);
+        }
+        // GET: Admin/EditRoles/userId
+        public async Task<IActionResult> EditRoles(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"Не удалось найти пользователя с ID '{userId}'.");
+            }
+
+            var allRoles = await _roleManager.Roles.ToListAsync();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var model = new EditRolesViewModel
+            {
+                UserId = user.Id,
+                UserEmail = user.Email,
+                UserRoles = userRoles,
+                AllRoles = allRoles.Select(r => new SelectListItem
+                {
+                    Text = r.Name,
+                    Value = r.Name
+                }).ToList()
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditRoles(EditRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user == null)
+            {
+                return NotFound($"Не удалось найти пользователя с ID '{model.UserId}'.");
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var resultRemove = await _userManager.RemoveFromRolesAsync(user, userRoles);
+            if (!resultRemove.Succeeded)
+            {
+                ModelState.AddModelError("", "Не удалось удалить существующие роли пользователя.");
+                return View(model);
+            }
+
+            model.SelectedRoles ??= new List<string>();
+            var resultAdd = await _userManager.AddToRolesAsync(user, model.SelectedRoles);
+            if (!resultAdd.Succeeded)
+            {
+                ModelState.AddModelError("", "Не удалось добавить новые роли пользователю.");
+                return View(model);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
